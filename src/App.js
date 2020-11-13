f55daab024492c066f0aa6cab69a1a1cdff6e974
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-11-13 14:45:23 +0330

    infinitescroll.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,6 @@
-/*import React, { useCallback, useRef, useState } from "react";
-import { UseNewsSearch } from "./UseNewsSearch";
+import React from "react";
 
-function App() {
-  const [query, setQuery] = useState("");
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const { news, loading, hasMore, error } = UseNewsSearch(query, pageNumber);
-  const observer = useRef();
-  const lastNewsElementRef = useCallback(
-    (node) => {
-      if (loading) return;
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          setPageNumber((prevPageNumber) => prevPageNumber + 1);
-        }
-      });
-      if (node) observer.current.observe(node);
-      console.log(node);
-    },
-    [loading, hasMore]
-  );
-
-  const handleSearch = (event) => {
-    setQuery(event.target.value);
-    setPageNumber(1);
-  };
-
-  return (
-    <div>
-      <input type="text" onChange={handleSearch} />
-      {news.map((item, index) => {
-        if (item.length === index + 1) {
-          return (
-            <div ref={lastNewsElementRef} key={item}>
-              {item}
-            </div>
-          );
-        } else {
-          return <div key={item}>{item}</div>;
-        }
-      })}
-      <div>{loading && "loading..."}</div>
-      <div>{error && "erroe..."}</div>
-    </div>
-  );
-}
-
-export default App;
-*/
-
-import React, { useState } from "react";
-import { NewsTitle } from "./NewsTitle";
+import { NewsTitle } from "./NewsTitle.js";
 
 function App() {
   return (
--- src/NewsTitle.js
+++ src/NewsTitle.js
@@ -1,52 +1,6 @@
-/*
-import { useEffect, useState } from "react";
-import axios from "axios";
-export const UseNewsSearch = (query, pageNumber) => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [news, setNews] = useState([]);
-  const [hasMore, setHasMore] = useState(false);
-
-  useEffect(() => {
-    setNews([]);
-  }, [query]);
-  useEffect(() => {
-    setLoading(true);
-    setError(false);
-    let cancel;
-    axios({
-      method: "Get",
-      url:
-        "http://newsapi.org/v2/everything?" +
-        "q=bitcoin&" +
-        "from=2020-11-12&" +
-        "sortBy=publishedAt&" +
-        "apiKey=a2f55d1f83be43af84d529e89a06117a",
-      params: { q: query, page: pageNumber },
-      cancelToken: new axios.CancelToken((c) => (cancel = c)),
-    })
-      .then((res) => {
-        setNews((prevNews) => {
-          return [
-            ...new Set([...prevNews, ...res.data.articles.map((b) => b.title)]),
-          ];
-        });
-        setHasMore(res.data.docs.length > 0);
-        setLoading(false);
-      })
-      .catch((e) => {
-        if (axios.isCancel(e)) return;
-        setError(true);
-      });
-    return () => cancel();
-  }, [query, pageNumber]);
-  return { loading, error, news, hasMore };
-};
-*/
-
 import React, { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-
+import styles from "./NewsTitle.module.css";
 export const NewsTitle = () => {
   const [news, setNews] = useState([]);
   const [hasMore, setHasMore] = useState(true);
@@ -62,7 +16,6 @@ export const NewsTitle = () => {
       .then((response) => response.json())
       .then((data) => setNews(data.articles));
   }, []);
-
   const fetchMoreData = () => {
     if (news.length >= 500) {
       setHasMore(false);
@@ -74,9 +27,9 @@ export const NewsTitle = () => {
       setNews(news.concat(Array.from({ length: 20 })));
     }, 500);
   };
-
   return (
     <div>
+      {" "}
       <InfiniteScroll
         dataLength={news.length} //This is important field to render the next data
         next={fetchMoreData}
@@ -89,9 +42,19 @@ export const NewsTitle = () => {
         }
         // below props only if you need pull down functionality
       >
-        {news.map((item) => (
-          <div> {item.title}</div>
-        ))}
+        {news.map((item) => {
+          return (
+            <div key={item.index} className={styles.news}>
+              <img
+                src={item.urlToImage}
+                alt={item.index}
+                className={styles.image}
+              />
+              <div className={styles.title}>{item.title}</div>
+              <div className={styles.title1}>{item.source.name}</div>
+            </div>
+          );
+        })}
       </InfiniteScroll>
     </div>
   );
