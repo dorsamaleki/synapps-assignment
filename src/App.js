5a5e47765c7989d54ae9f6f569ef52dcd7ececb0
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-11-13 10:10:12 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+/*import React, { useCallback, useRef, useState } from "react";
+import { UseNewsSearch } from "./UseNewsSearch";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { news, loading, hasMore, error } = UseNewsSearch(query, pageNumber);
+  const observer = useRef();
+  const lastNewsElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+      console.log(node);
+    },
+    [loading, hasMore]
+  );
+
+  const handleSearch = (event) => {
+    setQuery(event.target.value);
+    setPageNumber(1);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={handleSearch} />
+      {news.map((item, index) => {
+        if (item.length === index + 1) {
+          return (
+            <div ref={lastNewsElementRef} key={item}>
+              {item}
+            </div>
+          );
+        } else {
+          return <div key={item}>{item}</div>;
+        }
+      })}
+      <div>{loading && "loading..."}</div>
+      <div>{error && "erroe..."}</div>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+import React, { useState } from "react";
+import { UseNewsSearch } from "./UseNewsSearch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UseNewsSearch />
     </div>
   );
 }
---
+++ src/UseNewsSearch.js
@@ -0,0 +1,108 @@
+/*
+import { useEffect, useState } from "react";
+import axios from "axios";
+export const UseNewsSearch = (query, pageNumber) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [news, setNews] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+
+  useEffect(() => {
+    setNews([]);
+  }, [query]);
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    let cancel;
+    axios({
+      method: "Get",
+      url:
+        "http://newsapi.org/v2/everything?" +
+        "q=bitcoin&" +
+        "from=2020-11-12&" +
+        "sortBy=publishedAt&" +
+        "apiKey=a2f55d1f83be43af84d529e89a06117a",
+      params: { q: query, page: pageNumber },
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        setNews((prevNews) => {
+          return [
+            ...new Set([...prevNews, ...res.data.articles.map((b) => b.title)]),
+          ];
+        });
+        setHasMore(res.data.docs.length > 0);
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        setError(true);
+      });
+    return () => cancel();
+  }, [query, pageNumber]);
+  return { loading, error, news, hasMore };
+};
+*/
+
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const style = {
+  height: 30,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8,
+};
+
+export const UseNewsSearch = () => {
+  const [news, setNews] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  var url =
+    "http://newsapi.org/v2/everything?" +
+    "q=bitcoin&" +
+    "from=2020-11-12&" +
+    "sortBy=publishedAt&" +
+    "apiKey=a2f55d1f83be43af84d529e89a06117a";
+  var req = new Request(url);
+  useEffect(() => {
+    fetch(req)
+      .then((response) => response.json())
+      .then((data) => setNews(data.articles));
+  }, []);
+
+  const fetchMoreData = () => {
+    if (news.length >= 500) {
+      setHasMore(false);
+      return;
+    }
+    // a fake async api call like which sends
+    // 20 more records in .5 secs
+    setTimeout(() => {
+      setNews(news.concat(Array.from({ length: 20 })));
+    }, 500);
+  };
+
+  return (
+    <div>
+      <InfiniteScroll
+        dataLength={news.length} //This is important field to render the next data
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        height={400}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        // below props only if you need pull down functionality
+      >
+        {news.map((item, index) => (
+          <div style={style} key={index}>
+            {item.title}
+          </div>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
+};
