ebca9ed66568414d73ac1dc0aee0baca872d9a2a
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-11-15 00:25:27 +0330

    link

--- src/News.js
+++ src/News.js
@@ -1,13 +1,19 @@
 import React from "react";
 import styles from "./News.module.css";
 export const News = (props) => (
-  <div key={props.url} className={styles.news}>
+  <a
+    key={props.url}
+    className={styles.news}
+    href={props.url}
+    target="_blank"
+    rel="noreferrer"
+  >
     <div className={styles.left}>
       <div className={styles.title}>{props.title}</div>
-      <div className={styles.title1}>{props.source.name}</div>
+      <div className={styles.source}>{props.source.name}</div>
     </div>
     <div className={styles.image}>
       <img src={props.urlToImage} alt={props.index} />
     </div>
-  </div>
+  </a>
 );
